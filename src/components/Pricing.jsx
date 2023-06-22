2155c70a273c2c92834f0d21a1b07ab4331c8fc3
MatkoKardum <matejkardum1@gmail.com>
2023-06-22 14:42:46 +0200

    Fixed responsiveness of pricings section

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -17,27 +17,27 @@ function Pricing(props) {
       <div className="flex justify-center items-center">
         <div className="flex justify-center items-center flex-wrap px-2">
           <div
-            className={`flex justify-center items-center flex-col flex-1 border-solid border-[1px] py-8 px-10 mx-auto rounded-lg my-5 lg:my-0 mr-0 lg:mr-10  transition duration-150 ease-linear hover:scale-105 ${
+            className={`flex justify-center items-center w-full flex-col flex-1 border-solid border-[1px] py-5 px-8 lg:py-8 lg:px-10 mx-auto rounded-lg my-5 lg:my-0 mr-0 lg:mr-10  transition duration-150 ease-linear hover:scale-105 ${
               props.darkMode
                 ? "border-stone-400 bg-[#1f2937]"
                 : "border-white bg-white drop-shadow-2xl"
             }`}
           >
-            <h3 className="text-4xl font-bold">Starter</h3>
-            <h4
-              className={`font-medium text-xl my-4 text-center ${
+            <h3 className="text-xl lg:text-4xl font-bold">Starter</h3>
+            <p
+              className={`font-medium text-sm lg:text-xl my-2 lg:my-4 text-center ${
                 props.darkMode ? "text-gray-400" : "text-gray-800"
               }`}
             >
               Affordable custom website with responsive design.
-            </h4>
+            </p>
             <p
-              className={`mb-8 ${
+              className={`mb-4 lg:mb-8 ${
                 props.darkMode ? "text-gray-300" : "text-gray-700"
               }`}
             >
               <span
-                className={`text-6xl ${
+                className={`text-4xl lg:text-6xl ${
                   props.darkMode ? "text-white" : "text-black"
                 }`}
               >
@@ -46,55 +46,55 @@ function Pricing(props) {
               /month
             </p>
             <ul className="font-medium">
-              <li className="text-lg mb-2">
+              <li className="text-sm lg:text-lg mb-2">
                 <BsCheckLg className="inline-block text-green-400 mr-2" />
                 Custom Website Creation
               </li>
-              <li className="text-lg mb-2">
+              <li className="text-sm lg:text-lg mb-2">
                 <BsCheckLg className="inline-block text-green-400 mr-2" />
                 Responsive Design
               </li>
-              <li className="text-lg mb-2">
+              <li className="text-sm lg:text-lg mb-2">
                 <BsCheckLg className="inline-block text-green-400 mr-2" />
                 SEO Optimization
               </li>
-              <li className="text-lg mb-2">
+              <li className="text-sm lg:text-lg mb-2">
                 <BsCheckLg className="inline-block text-green-400 mr-2" />
                 Content Management System
               </li>
-              <li className="text-lg mb-2">
+              <li className="text-sm lg:text-lg mb-2">
                 <BsCheckLg className="inline-block text-green-400 mr-2" />
                 Ongoing Support
               </li>
             </ul>
             <button
-              className={`w-full bg-blue-600 rounded-md text-xl py-2 mt-4  hover:bg-blue-800 hover:border-blue-800 transition duration-200 active:bg-blue-600 active:border-blue-600 text-white`}
+              className={`w-full bg-blue-600 rounded-md text-md lg:text-xl py-1 lg:py-2 mt-4  hover:bg-blue-800 hover:border-blue-800 transition duration-200 active:bg-blue-600 active:border-blue-600 text-white`}
             >
               Get started
             </button>
           </div>
           <div
-            className={`flex justify-center items-center flex-col flex-1 border-solid border-[1px] py-8 px-10 mx-auto rounded-lg my-5 lg:my-0 mr-0 lg:mr-10  transition duration-150 ease-linear hover:scale-105 ${
+            className={`flex justify-center items-center flex-col flex-1 border-solid border-[1px] py-5 px-8 lg:py-8 lg:px-10 mx-auto rounded-lg my-5 lg:my-0 mr-0 lg:mr-10  transition duration-150 ease-linear hover:scale-105 ${
               props.darkMode
                 ? "border-stone-400 bg-[#1f2937]"
                 : "border-white bg-white drop-shadow-2xl"
             }`}
           >
-            <h3 className="text-4xl font-bold">Professional</h3>
-            <h4
-              className={`font-medium text-xl my-4 text-center ${
+            <h3 className="text-xl lg:text-4xl font-bold">Professional</h3>
+            <p
+              className={`font-medium text-sm lg:text-xl my-2 lg:my-4 text-center ${
                 props.darkMode ? "text-gray-400" : "text-gray-800"
               }`}
             >
               Advanced e-commerce integration and premium design.
-            </h4>
+            </p>
             <p
-              className={`mb-8 ${
+              className={`mb-4 lg:mb-8 ${
                 props.darkMode ? "text-gray-300" : "text-gray-700"
               }`}
             >
               <span
-                className={`text-6xl ${
+                className={`text-4xl lg:text-6xl ${
                   props.darkMode ? "text-white" : "text-black"
                 }`}
               >
@@ -103,53 +103,53 @@ function Pricing(props) {
               /month
             </p>
             <ul className="font-medium">
-              <li className="text-lg mb-2">
+              <li className="text-sm lg:text-lg mb-2">
                 <BsCheckLg className="inline-block text-green-400 mr-2" />
                 E-commerce Integration{" "}
               </li>
-              <li className="text-lg mb-2">
+              <li className="text-sm lg:text-lg mb-2">
                 <BsCheckLg className="inline-block text-green-400 mr-2" />{" "}
                 Payment Gateway Integration
               </li>
-              <li className="text-lg mb-2">
+              <li className="text-sm lg:text-lg mb-2">
                 <BsCheckLg className="inline-block text-green-400 mr-2" />{" "}
                 Product Catalog Management
               </li>
-              <li className="text-lg mb-2">
+              <li className="text-sm lg:text-lg mb-2">
                 <BsCheckLg className="inline-block text-green-400 mr-2" />
                 Order Tracking
               </li>
-              <li className="text-lg mb-2">
+              <li className="text-sm lg:text-lg mb-2">
                 <BsCheckLg className="inline-block text-green-400 mr-2" />{" "}
                 Conversion Optimization
               </li>
             </ul>
-            <button className="w-full text-white bg-blue-600 rounded-md text-xl py-2 mt-4  hover:bg-blue-800 hover:border-blue-800 transition duration-200 active:bg-blue-600 active:border-blue-600">
+            <button className="w-full text-white bg-blue-600 rounded-md text-md lg:text-xl py-1 lg:py-2 mt-4  hover:bg-blue-800 hover:border-blue-800 transition duration-200 active:bg-blue-600 active:border-blue-600">
               Get started
             </button>
           </div>
           <div
-            className={`flex justify-center items-center flex-1 flex-col border-solid border-[1px] py-8 px-10 mx-auto rounded-lg my-5 lg:my-0 mr-0 lg:mr-10 transition duration-150 ease-linear hover:scale-105 ${
+            className={`flex justify-center items-center flex-1 flex-col border-solid border-[1px] py-5 px-8 lg:py-8 lg:px-10 mx-auto rounded-lg my-5 lg:my-0 mr-0 lg:mr-10 transition duration-150 ease-linear hover:scale-105 ${
               props.darkMode
                 ? "border-stone-400 bg-[#1f2937]"
                 : "border-white bg-white drop-shadow-2xl"
             }`}
           >
-            <h3 className="text-4xl font-bold">Enterprise</h3>
-            <h4
-              className={`font-medium text-xl my-4 text-center ${
+            <h3 className="text-xl lg:text-4xl font-bold">Enterprise</h3>
+            <p
+              className={`font-medium text-sm lg:text-xl my-2 lg:my-4 text-center ${
                 props.darkMode ? "text-gray-400" : "text-gray-800"
               }`}
             >
               Customized solutions with optimization and support.{" "}
-            </h4>
+            </p>
             <p
-              className={`mb-8  text-center ${
+              className={`mb-4 lg:mb-8 ${
                 props.darkMode ? "text-gray-300" : "text-gray-700"
               }`}
             >
               <span
-                className={`text-6xl ${
+                className={`text-4xl lg:text-6xl ${
                   props.darkMode ? "text-white" : "text-black"
                 }`}
               >
@@ -158,28 +158,28 @@ function Pricing(props) {
               /month
             </p>
             <ul className="font-medium">
-              <li className="text-lg mb-2">
+              <li className="text-sm lg:text-lg mb-2">
                 <BsCheckLg className="inline-block text-green-400 mr-2" />
                 Premium Design Templates{" "}
               </li>
-              <li className="text-lg mb-2">
+              <li className="text-sm lg:text-lg mb-2">
                 <BsCheckLg className="inline-block text-green-400 mr-2" />
                 Dedicated Project Manager
               </li>
-              <li className="text-lg mb-2">
+              <li className="text-sm lg:text-lg mb-2">
                 <BsCheckLg className="inline-block text-green-400 mr-2" />{" "}
                 Custom Development
               </li>
-              <li className="text-lg mb-2">
+              <li className="text-sm lg:text-lg mb-2">
                 <BsCheckLg className="inline-block text-green-400 mr-2" />
                 Performance Optimization
               </li>
-              <li className="text-lg mb-2">
+              <li className="text-sm lg:text-lg mb-2">
                 <BsCheckLg className="inline-block text-green-400 mr-2" />
                 Website Maintenance
               </li>
             </ul>
-            <button className="w-full text-white bg-blue-600 rounded-md text-xl py-2 mt-4  hover:bg-blue-800 hover:border-blue-800 transition duration-200 active:bg-blue-600 active:border-blue-600">
+            <button className="w-full text-white bg-blue-600 rounded-md text-md lg:text-xl py-1 lg:py-2 mt-4  hover:bg-blue-800 hover:border-blue-800 transition duration-200 active:bg-blue-600 active:border-blue-600">
               Get started
             </button>
           </div>
